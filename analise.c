0b445d222decfeb2481d7b1defd9fa99ff0b49a2
ViCor44 <victor.a.correia@gmail.com>
2018-05-03 23:02:46 +0100

    Update analise.c

--- analise.c
+++ analise.c
@@ -1,7 +1,15 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "header.h"
+void InserirComando()
+{
+	char comando[16];
+	CriaLinha();
+    gotoxy(2, 2);
+    printf(" Insira comando: ");
+	gets(comando);
+	AnalisaTexto(comando);
+}
 
 void AnalisaTexto(char *comando)
 {
@@ -12,6 +20,8 @@ void AnalisaTexto(char *comando)
 	y = atoi(strdup(strtok(NULL, " ,+")));
 	w = atoi(strdup(strtok(NULL, " ,+")));
 	h = atoi(strdup(strtok(NULL, " ,+")));
+	y = (40-y-h)+1;
+	
 	
 	if(strcmp(ordem, "create") == 0)
 		CriarRetangulo(x, y, w, h);
