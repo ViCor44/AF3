57d3b5b7f95b601607e29a08b4ee08354b9ce2c7
ViCor44 <victor.a.correia@gmail.com>
2018-05-03 23:05:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,13 @@
-#include<windows.h>
-#include<stdio.h>
-#include<stdlib.h>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "header.h"
 
-int main(void){
-	int i;	
-	char comando[16];		
+int main()
+{
     Configtela();
-    CriaLinha();
-    gotoxy(2, 2);
-    printf(" Insira comando: ");
-	gets(comando);
-	AnalisaTexto(comando);		
+    remove_scrollbar();	
+    InserirComando();			
     system("pause >nul");
     return 0;
 }
