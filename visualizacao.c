54b4c6975533027103df73ef025ac0b941bee234
ViCor44 <victor.a.correia@gmail.com>
2018-05-03 20:48:03 +0100

    Create visualizacao.c

---
+++ visualizacao.c
@@ -0,0 +1,66 @@
+#include<windows.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+/* Função q configura o tamanho da tela e do buffer*/
+void Configtela()
+{
+COORD outbuff;
+outbuff.X = 150;
+outbuff.Y = 50;
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+SetConsoleScreenBufferSize(hConsole, outbuff);
+Sleep(130);
+SMALL_RECT windowSize = {0, 0, 149, 49}; 
+SetConsoleWindowInfo(hConsole, TRUE, &windowSize);
+}
+
+void gotoxy(int x, int y){
+     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){x-1,y-1});
+}
+/* Cria uma linha para separar a insercao de comandos*/
+void CriaLinha()
+{
+	int i;
+	for(i= 1; i < 150; i++)
+    {
+		gotoxy(i, 3);
+		printf("-");
+	}    
+}
+
+/* Funcao para criar retangulos*/
+void CriarRetangulo(int x, int y, int l, int h)
+{	
+	int i;
+	system("@cls||clear");
+    gotoxy(x,y);
+    printf("+");
+    for(i= 1; i < l - 1; i++)
+    {
+		gotoxy(x + i,y);
+		printf("-");
+	}    	
+    gotoxy(x + l - 1,y);    
+    printf("+");
+    for(i= 1; i < h - 1; i++)
+    {
+		gotoxy(x + l -1, y + i);
+    	printf("|");
+	}
+    gotoxy(x,y + h - 1);
+    printf("+");
+    for(i= 1; i < l - 1; i++)
+    {
+		gotoxy(x + i, y + h -1);
+		printf("-");
+	}    
+	for(i= 1; i < h - 1; i++)
+    {
+		gotoxy(x, y + i);
+    	printf("|");
+	}  
+	gotoxy(x + l - 1,y + h - 1);
+    printf("+\n");
+    Sleep(1);
+}
