5e182493ce0830e8701456af70bd07d5b4c7d551
ViCor44 <victor.a.correia@gmail.com>
2018-05-03 23:03:36 +0100

    Update header.h

--- header.h
+++ header.h
@@ -6,5 +6,7 @@ void gotoxy(int x, int y);
 void CriaLinha();
 void CriarRetangulo(int x, int y, int l, int h);
 void AnalisaTexto(char *comando);
+void InserirComando();
+void remove_scrollbar();
 
 #endif /* HEADER_H_ */
