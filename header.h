1872ee99faec81c7affd1b38079bd8f7a4bfc96a
ViCor44 <victor.a.correia@gmail.com>
2018-05-03 20:48:52 +0100

    Create header.h

---
+++ header.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H_
+#define HEADER_H_
+
+void Configtela();
+void gotoxy(int x, int y);
+void CriaLinha();
+void CriarRetangulo(int x, int y, int l, int h);
+void AnalisaTexto(char *comando);
+
+#endif /* HEADER_H_ */
