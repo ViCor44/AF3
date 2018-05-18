3eec4c5c23897aafe510b272b409b548d32433a2
ViCor44 <victor.a.correia@gmail.com>
2018-05-18 21:55:36 +0100

    Update header.h

--- header.h
+++ header.h
@@ -1,15 +1,27 @@
 #ifndef HEADER_H_
 #define HEADER_H_
 
+typedef struct sRets{
+    	int x;
+    	int y;
+    	int l;
+    	int h;
+    	struct sRets *next ;
+} Rets;
+
+
+void init();
 void Configtela();
 void gotoxy(int x, int y);
 void CriaLinha();
-void CriarRetangulo(int x, int y, int l, int h);
+void CriarRetangulo(int x, int y, int l, int h, int modo);
 void MoverEsquerda(int x, int y, int ml);
 void MoverDireita(int x, int y, int mr);
 void ApagarRetangulo(int x, int y);
 void AnalisaTexto(char *comando);
 void InserirComando();
 void remove_scrollbar();
+void gravity();
+void release();
 
 #endif /* HEADER_H_ */
